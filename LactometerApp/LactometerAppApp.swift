4ee8e6212cccc1c87bfa3af91d87e108accce9ba
Viktor Ovsienko <vovsienko@ptsecurity.com>
2025-02-01 20:20:44 +0300

    Initial Commit

---
+++ LactometerApp/LactometerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LactometerAppApp.swift
+//  LactometerApp
+//
+//  Created by Виктор Овсиенко on 01.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct LactometerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LactometerAppTests/LactometerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LactometerAppTests.swift
+//  LactometerAppTests
+//
+//  Created by Виктор Овсиенко on 01.02.2025.
+//
+
+import Testing
+@testable import LactometerApp
+
+struct LactometerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
